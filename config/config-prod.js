9a9d4e0abef71266c7bde7a24e9ade9b98f4fe77
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-09 20:39:38 +0800

    生产者配置项

---
+++ config/config-prod.js
@@ -0,0 +1,12 @@
+// 开发者环境
+
+const configProd = {
+    database: 'design-project', // 使用哪个数据库
+    username: 'root', // 用户名
+    password: '123456', // 口令
+    host: 'localhost', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+// 暴露配置
+module.exports = configProd;
