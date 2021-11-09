2a987d5f225f7c5a7f5a01abe4844a6886cf816a
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-09 20:40:01 +0800

    判断是开发者还是生产者

---
+++ config/config.js
@@ -0,0 +1,18 @@
+// 实际的配置环境
+
+// 生产环境
+const configDev = './config-dev'
+
+// 上线环境
+const configProd = './config-prod'
+
+let config = ''
+// 判断环境
+if (process.env.NODE_ENV === 'development') {
+    config = require(configDev)
+} else {
+    config = require(configProd)
+}
+
+// 暴露
+module.exports = config;
