a12d0df1c54dc08c582536720c3209be0b2df24a
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-09 20:40:35 +0800

    实例化连接工具

---
+++ config/db.js
@@ -0,0 +1,38 @@
+const {
+    Sequelize
+} = require('sequelize');
+// 连接数据库
+const config = require('./config')
+
+// 配置数据库连接
+const sequelizedb = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    // 连接池设置
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    },
+
+    // 自定义日志
+    logging: function (message) {
+        console.debug(message);
+    },
+    // 全局配置表名和Model名称一致,全局禁用时间戳
+    define: {
+        freezeTableName: true,
+        timestamps: false
+    }
+});
+
+// 测试连接数据库是否成功
+try {
+    sequelizedb.authenticate();
+    console.log('数据库连接成功');
+} catch (error) {
+    console.error('无法连接数据库，请检查重试:', error);
+};
+
+// 暴露配置
+module.exports = sequelizedb;
