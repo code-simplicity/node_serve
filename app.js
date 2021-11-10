4f95cab2bb7ef3860d029dad6c3050a4a07d34d4
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-10 15:08:50 +0800

    配置jwt，实现登录认证，修复跨端请求的多次请求响应

--- app.js
+++ app.js
@@ -4,18 +4,33 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require("cors");
+
+const expressJWT = require('express-jwt');
+
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+// 导入jwtUtils
+const jwtUtils = require('./utils/jwtUtils');
 
 const app = express();
+
+// 私有秘钥
+const PRIVITE_KEY = 'bugdr_token'
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 处理跨域
+app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,19 +38,61 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// 解析token，获取用户信息
+app.use(function (req, res, next) {
+  const token = req.headers['authorization']
+  console.log(`token  object`, token)
+  if (token === undefined) {
+    // 直接退出，避免多次请求
+    return
+  } else {
+    jwtUtils.verToken(token).then((data) => {
+      req.data = data
+      return next()
+    }).catch((err) => {
+      console.log(`err`, err)
+      // 直接退出，避免多次请求
+      return
+    })
+  }
+})
+
+// 验证token是否过期，并且规定哪一些路由是不需要token的
+app.use(expressJWT({
+  // 设置令牌
+  secret: PRIVITE_KEY,
+  // 设置加密算法
+  algorithms: ['HS256'],
+  // 校验是否存在token，有token才可以访问
+  credentialsRequired: true
+}).unless({
+  path: ['/user/login', '/user/add', '/user/logout']
+}))
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // 判断token是否存在
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({
+      status: 201,
+      msg: 'token验证失败，请重新登录.'
+    })
+    return
+  } else {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+    // 直接退出，避免多次请求
+    return
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
