c8d240e2d1f9a8b6a8241a70e1bf79dd3f1dd3c5
Romantic <dupyi0912@gmail.com>
2022-03-23 17:23:47 +0800

    fix: 邮箱发送功能成功

--- utils/EmailSendUtils.js
+++ utils/EmailSendUtils.js
@@ -1,5 +1,5 @@
 // 邮件发送的工具
-const nodeMailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 // 引入qq授权码和qq邮箱
 const {
@@ -9,30 +9,40 @@ const {
 const config = {
     host: partnerEmail.QQ.host,
     port: partnerEmail.QQ.port,
-    auto: {
+    secureConnection: true,
+    secure: true,
+    auth: {
         user: partnerEmail.QQ.user,
         pass: partnerEmail.QQ.pass,
     }
 }
 
 // 发送邮件的插件
-const sendEmail = (email, code) => {
+const sendEmail = (emailAddress, code) => {
     // 发送验证码
-    const transporter = nodeMailer.createTransport(config)
+    const transporter = nodemailer.createTransport(config)
     const mail = {
-        from: "水运工程系统",
-        subject: "邮箱验证码",
-        to: email,
+        from: "test<468264345@qq.com>", // 发送邮箱
+        subject: "水运工程-邮箱验证码", // 主题
+        to: emailAddress, // 去的地址
         // 内容
         text: `您的验证码为:${code}, 五分钟之内有效，请谨慎保管。`,
-        // 添加html标签
-        html: '<a href="https://www.baidu.com.com">baidu</a>'
     }
-    // 发送验证码
     return new Promise((resolve, reject) => {
-        transporter.sendMail(mail, (error, info) => {
-            error ? reject(error) : resolve(info.response)
-            transporter.close()
+        transporter.sendMail(mail, (err, res) => {
+            try {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(res.response)
+                }
+                // 关闭连接
+                transporter.close()
+            } catch (error) {
+                reject(err)
+                // 关闭连接
+                transporter.close()
+            }
         })
     })
 }
