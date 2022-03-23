9261efeb90e72fa1144b2c1b644aef4d2331757a
Romantic <dupyi0912@gmail.com>
2022-03-23 20:16:34 +0800

    feat: 测试邮件发送的代码

--- routes/test/code.js
+++ routes/test/code.js
@@ -7,8 +7,6 @@ const {
 
 const R = require("../../utils/R")
 
-const utils = require("../../utils/utils");
-
 const EmailSendUtils = require("../../utils/EmailSendUtils.js")
 
 router.post("/sendMail", async (req, res) => {
@@ -16,7 +14,6 @@ router.post("/sendMail", async (req, res) => {
         code,
         emaillAddress
     } = req.body
-
     const data = await EmailSendUtils.sendEmail(emaillAddress, code)
     if (data) {
         return res.send(R.success(data, "发送验证码成功."))
