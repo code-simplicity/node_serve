1f0db3da1235baad0a0df25c3664992f26c5c36c
Romantic <dupyi0912@gmail.com>
2022-03-23 17:22:59 +0800

    fix: 添加测试代码的接口

--- routes/test/code.js
+++ routes/test/code.js
@@ -5,19 +5,24 @@ const {
     Op
 } = require("sequelize");
 
-const R = require("../utils/R")
+const R = require("../../utils/R")
 
-const utils = require("../utils/utils");
+const utils = require("../../utils/utils");
 
 const EmailSendUtils = require("../../utils/EmailSendUtils.js")
 
 router.post("/sendMail", async (req, res) => {
     const {
         code,
-        email
+        emaillAddress
     } = req.body
-    const data = await EmailSendUtils.sendEmail(email, code)
+
+    const data = await EmailSendUtils.sendEmail(emaillAddress, code)
     if (data) {
         return res.send(R.success(data, "发送验证码成功."))
+    } else {
+        return res.send(R.fail("发送验证码失败."))
     }
 })
+
+module.exports = router;
