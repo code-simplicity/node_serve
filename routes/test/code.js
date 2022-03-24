a552bce07a9f6ef795c9a4e7d375fe879f54a936
Romantic <dupyi0912@gmail.com>
2022-03-24 13:05:19 +0800

    feat: 编写发送验证码的测试用例

--- routes/test/code.js
+++ routes/test/code.js
@@ -7,18 +7,45 @@ const {
 
 const R = require("../../utils/R")
 
+const utils = require("../../utils/utils")
+
+const redis = require("../../config/redis")
+
 const EmailSendUtils = require("../../utils/EmailSendUtils.js")
 
+/**
+ * 测试发送邮件
+ */
 router.post("/sendMail", async (req, res) => {
     const {
         code,
-        emaillAddress
+        emailAddress
     } = req.body
-    const data = await EmailSendUtils.sendEmail(emaillAddress, code)
+    const data = await EmailSendUtils.sendEmail(emailAddress, code)
+    const remoteAddr = req.ip
+    const ipKey = "email_key_" + remoteAddr
+    // 防止被频繁调用，这里通过ip地址进行拦截
+    const ipTime = redis.getString(ipKey)
+    if (!utils.isEmpty(ipTime)) {
+        let i = parseInt(ipTime)
+        console.log(`当前ip${remoteAddr}调用了${i}次`)
+        if (i > 10) {
+            return res.send(R.fail("请不要频繁发送验证码，否则将被拉黑."))
+        } else {
+            i++
+            redis.setString(ipKey, i, )
+        }
+    }
+    const redisResult = await redis.setString(ipKey, code, 60 * 1)
     if (data) {
-        return res.send(R.success(data, "发送验证码成功."))
+        return res.send(R.success({
+            data,
+            redisResult
+        }, "发送验证码成功."))
     } else {
-        return res.send(R.fail("发送验证码失败."))
+        return res.send(R.fail({
+            redisResult
+        }, "发送验证码失败."))
     }
 })
 
