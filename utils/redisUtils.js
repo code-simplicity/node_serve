51bcfe645db9d329faa3bb23ec893186d99c70aa
Romantic <dupyi0912@gmail.com>
2022-03-24 13:06:19 +0800

    feat: 编写redis相关工具，比如get和set

--- utils/redisUtils.js
+++ utils/redisUtils.js
@@ -1,3 +1,46 @@
 /**
  * redis工具
  */
+
+const {
+    redisClient
+} = require("../config/redis")
+
+/**
+ * 
+ * @param {*} key 存放的关键字
+ * @param {*} value 具体的值
+ * @param {*} expire 过期时间
+ * @returns 
+ */
+const setString = (key, value, expire) => {
+    return new Promise((resolve, reject) => {
+        redisClient.set(key, value, (err, res) => {
+            if (err) {
+                console.log("err", err)
+                reject(err)
+            }
+            if (!isNaN(expire) && expire > 0) {
+                redisClient.expire(key, parseInt(expire))
+            }
+            resolve(res)
+        })
+    })
+}
+
+const getString = (key) => {
+    return new Promise((resolve, reject) => {
+        redisClient.get(key, (err, res) => {
+            if (err) {
+                console.log("err", err)
+                reject(err)
+            }
+            resolve(res)
+        })
+    })
+}
+
+module.exports = {
+    setString,
+    getString
+}
