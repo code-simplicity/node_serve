0f3fd3be96692d4ed03dba9f00798403f1286c01
Romantic <dupyi0912@gmail.com>
2022-03-30 19:40:52 +0800

    对异常进行判断

--- controller/portal/waveforms.js
+++ controller/portal/waveforms.js
@@ -9,18 +9,22 @@ const resCode = require("../../utils/resCode");
 
 const waveforms = {
     async waveFormsByPointIdFindOne(args, res) {
-        const {
-            point_id
-        } = args
-        if (!point_id) {
-            return res.send(new FailModel("point_id不能为空"))
-        }
-        const {
-            dataValues
-        } = await waveformsServer.waveFormsByPointIdFindOne(point_id)
-        if (dataValues !== null) {
-            return res.send(new SuccessModel(dataValues, "查询波形图成功"))
-        } else {
+        try {
+            const {
+                point_id
+            } = args
+            if (!point_id) {
+                return res.send(new FailModel("point_id不能为空"))
+            }
+            const {
+                dataValues
+            } = await waveformsServer.waveFormsByPointIdFindOne(point_id)
+            if (dataValues !== null) {
+                return res.send(new SuccessModel(dataValues, "查询波形图成功"))
+            } else {
+                return res.send(new FailModel("查询波形图失败"))
+            }
+        } catch (error) {
             return res.send(new FailModel("查询波形图失败"))
         }
     }
--- controller/portal/wavestats.js
+++ controller/portal/wavestats.js
@@ -9,18 +9,22 @@ const resCode = require("../../utils/resCode");
 
 const wavestats = {
     async wavestatsByPointIdFindOne(args, res) {
-        const {
-            point_id
-        } = args
-        if (!point_id) {
-            return res.send(new FailModel("point_id不能为空"))
-        }
-        const {
-            dataValues
-        } = await wavestatsServer.wavestatsByPointIdFindOne(point_id)
-        if (dataValues !== null) {
-            return res.send(new SuccessModel(dataValues, "查询波形统计图成功"))
-        } else {
+        try {
+            const {
+                point_id
+            } = args
+            if (!point_id) {
+                return res.send(new FailModel("point_id不能为空"))
+            }
+            const {
+                dataValues
+            } = await wavestatsServer.wavestatsByPointIdFindOne(point_id)
+            if (dataValues !== null) {
+                return res.send(new SuccessModel(dataValues, "查询波形统计图成功"))
+            } else {
+                return res.send(new FailModel("查询波形统计图失败"))
+            }
+        } catch (error) {
             return res.send(new FailModel("查询波形统计图失败"))
         }
     }
